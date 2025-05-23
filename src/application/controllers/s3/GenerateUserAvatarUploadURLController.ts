908c7f298c175d50dddc11af3690466ea5edf24c
oliveiradniel <kadadniel@gmail.com>
2025-05-22 21:52:02 -0300

    feat: create a controller to receive the request and return the pre-signed url and the image key/name

---
+++ src/application/controllers/s3/GenerateUserAvatarUploadURLController.ts
@@ -0,0 +1,25 @@
+import { IController, IRequest, IResponse } from '../../interfaces/IController';
+
+import { generateUserAvatarUploadURL } from '../../../server/s3/generateUserAvatarUploadURL';
+
+export class GenerateUserAvatarUploadURLController implements IController {
+  async handle(request: IRequest): Promise<IResponse> {
+    try {
+      const mimeType = request.queryParams?.type as string;
+
+      const { url, key } = await generateUserAvatarUploadURL(mimeType);
+
+      return {
+        statusCode: 200,
+        body: { url, key },
+      };
+    } catch (error) {
+      console.log(error);
+
+      return {
+        statusCode: 400,
+        body: null,
+      };
+    }
+  }
+}
