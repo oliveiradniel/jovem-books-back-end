81db2e56e650bab867717ec9912933f483e49e57
oliveiradniel <kadadniel@gmail.com>
2025-05-26 19:02:10 -0300

    feat: allow multiple origins in CORS middleware

--- src/server/middlewares/cors.ts
+++ src/server/middlewares/cors.ts
@@ -1,9 +1,21 @@
 import { NextFunction, Request, Response } from 'express';
 
 export function cors(request: Request, response: Response, next: NextFunction) {
-  response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-  response.setHeader('Access-Control-Allow-Methods', '*');
-  response.setHeader('Access-Control-Allow-Headers', '*');
+  const allowedOrigins = [
+    'http://localhost:5173',
+    'jovem-books-front-end.vercel.app',
+  ];
+
+  const origin = request.header('origin')!;
+
+  const isAllowed = allowedOrigins.includes(origin);
+
+  if (isAllowed) {
+    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    response.setHeader('Access-Control-Allow-Methods', '*');
+    response.setHeader('Access-Control-Allow-Headers', '*');
+    response.setHeader('Access-Control-Max-Age', '10');
+  }
 
   next();
 }
