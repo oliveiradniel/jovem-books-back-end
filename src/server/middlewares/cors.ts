3fa535aeec787abcaca21f412d6802991fe3a614
oliveiradniel <kadadniel@gmail.com>
2025-05-26 20:32:17 -0300

    refactor: fix bug and allow origin instead of fixed value

--- src/server/middlewares/cors.ts
+++ src/server/middlewares/cors.ts
@@ -9,12 +9,11 @@ export function cors(request: Request, response: Response, next: NextFunction) {
   ];
 
   const origin = request.header('origin')!;
-  console.log(origin);
 
   const isAllowed = allowedOrigins.includes(origin);
 
   if (isAllowed) {
-    response.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+    response.setHeader('Access-Control-Allow-Origin', origin);
     response.setHeader('Access-Control-Allow-Methods', '*');
     response.setHeader('Access-Control-Allow-Headers', '*');
     response.setHeader('Access-Control-Max-Age', '10');
